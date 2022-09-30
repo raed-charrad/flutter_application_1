fb254ddfcf435007dff6a26c152192a8624870f4
raed charrad <raed.charrad91@gmail.com>
2022-09-30 11:02:36 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(child: Text('I am rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
